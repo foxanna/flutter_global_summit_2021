870f5077d129f5761ec6bfe9f1e9e2a1656c3638
Anna Domashych <anna.domashych@gmail.com>
2022-07-21 12:12:05 -0500

    05. AppLocalizations and delegate

---
+++ conference_schedule/lib/localization/app_localizations.dart
@@ -0,0 +1,38 @@
+import 'package:conference_schedule/localization/translation/translations.i69n.dart';
+import 'package:conference_schedule/localization/translation/translations_fr.i69n.dart';
+import 'package:conference_schedule/localization/translation/translations_uk.i69n.dart';
+import 'package:flutter/widgets.dart';
+
+final _translations = <String, Translations Function()>{
+  'en': () => const Translations(),
+  'fr': () => const Translations_fr(),
+  'uk': () => const Translations_uk(),
+};
+
+class AppLocalizations {
+  const AppLocalizations(this.translations);
+
+  final Translations translations;
+
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  static final List<Locale> supportedLocales =
+      _translations.keys.map((x) => Locale(x)).toList();
+}
+
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) =>
+      _translations.keys.contains(locale.languageCode);
+
+  @override
+  Future<AppLocalizations> load(Locale locale) =>
+      Future.value(AppLocalizations(_translations[locale.languageCode]!()));
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) => false;
+}
